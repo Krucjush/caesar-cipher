a1b505af352561aa2a26cc9e5753de42438f88f9
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-10-07 22:02:14 +0200

    Reorganise code. Remove repetitiveness.

--- main.py
+++ main.py
@@ -3,30 +3,21 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 if direction != 'encode' and direction != 'decode':
     exit(f'Unknown command {direction}.\nAborting.')
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+text = input('Type your message:\n').lower()
+shift = int(input('Type the shift number:\n'))
 
-def encrypt(plain_text, shift_amount):
-    encoded_text = ''
-    for letter in plain_text:
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ''
+    if cipher_direction == 'decode':
+        shift_amount *= -1
+    for letter in start_text:
         position = alphabet.index(letter)
         new_position = position + shift_amount
         if new_position >= len(alphabet):
             new_position -= len(alphabet)
-        encoded_text += alphabet[new_position]
-    print(f"The encoded text is {encoded_text}")
-
-def decrypt(plain_text, shift_amount):
-    decoded_text = ''
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
         if new_position < 0:
             new_position += len(alphabet)
-        decoded_text += alphabet[new_position]
-    print(f"The decoded text is {decoded_text}")
+        end_text += alphabet[new_position]
+    print(f'The {cipher_direction}d text is {end_text}')
 
-if direction == 'encode':
-    encrypt(text, shift)
-elif direction == 'decode':
-    decrypt(text, shift)
+caesar(text, shift, direction)
