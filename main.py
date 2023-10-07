23849af9cd1f09c758feca7a1afd4c5187a8e674
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-10-07 22:22:09 +0200

    User experience improvements & final touches

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
+import art
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-if direction != 'encode' and direction != 'decode':
-    exit(f'Unknown command {direction}.\nAborting.')
-text = input('Type your message:\n').lower()
-shift = int(input('Type the shift number:\n'))
+print(art.logo)
+
+
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ''
     if cipher_direction == 'decode':
         shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        if new_position >= len(alphabet):
-            new_position -= len(alphabet)
-        if new_position < 0:
-            new_position += len(alphabet)
-        end_text += alphabet[new_position]
-    print(f'The {cipher_direction}d text is {end_text}')
+    for character in start_text:
+        if character in alphabet:
+            position = alphabet.index(character)
+            new_position = position + shift_amount
+            if new_position >= len(alphabet):
+                new_position -= len(alphabet)
+            if new_position < 0:
+                new_position += len(alphabet)
+            end_text += alphabet[new_position]
+        else:
+            end_text += character
+    print(f"Here's the {cipher_direction}d result: {end_text}")
 
-caesar(text, shift, direction)
+run = True
+while run:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    if direction != 'encode' and direction != 'decode':
+        exit(f'Unknown command {direction}.\nAborting.')
+    text = input('Type your message:\n').lower()
+    shift = int(input('Type the shift number:\n'))
+    
+    caesar(text, shift, direction)
+    go_again = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if go_again == 'no':
+        run = False
+        print("Goodbye")
